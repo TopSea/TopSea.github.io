90fef797a32c150c4263d45f96a9c1e46a4e0e87
TopSea <cgbdyw@gmail.com>
2021-12-27 20:23:16 +0800

    调整了博客内容页
    视频高度与宽度的比例，内容整体宽度和边距。

---
+++ assets/js/sea-write.js
@@ -0,0 +1,5 @@
+var video = document.querySelector("#videoB");
+var currWidth = video.clientWidth;
+var height = currWidth / 4 * 3;
+video.style.height = height + "px";
+// video.style.height = 50 + "px";
